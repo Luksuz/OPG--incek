55d171a7a4b6b45b6a3ed07225c7a0e96d1d39ce
Luksuz <lukamindjek@gmail.com>
2023-09-08 11:31:38 +0200

    modals & form sadded

--- src/components/plantCard.js
+++ src/components/plantCard.js
@@ -1,27 +1,46 @@
 import React, { useState } from "react";
+import { Modal, Button, Form, Row, Col, InputGroup } from "react-bootstrap";
 import { motion } from "framer-motion";
 
 export default function PlantCard({ imageUrl, name, price, description }) {
   const [show, setShow] = useState(false);
+  const [validated, setValidated] = useState(false);
 
-  const handleOpen = () => {
+  const handleShow = () => {
+    console.log("show");
     setShow(true);
   };
 
   const handleClose = () => {
+    console.log("close");
     setShow(false);
+  };
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
   };
 
   return (
     <motion.div
       className="card shadow-lg"
-      initial={{ opacity: 0, scale: 0.5}}
+      initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, transition: { duration: 0.2 } }}
       style={{ width: "18rem" }}
-      onClick={handleOpen}
     >
-      <img src={imageUrl} className="card-img-top img-fluid" alt={name}></img>
+      <img
+        src={imageUrl}
+        className="card-img-top img-fluid"
+        alt={name}
+        onClick={handleShow}
+      />
+
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <hr />
@@ -31,52 +50,107 @@ export default function PlantCard({ imageUrl, name, price, description }) {
       </div>
 
       {/* Modal */}
-      {show && (
-        <motion.div
-          className="modal fade"
-          id="exampleModal"
-          tabIndex="-1"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.5,
-            ease: [0.16, 1, 0.3, 1],
-          }}
-        >
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">
-                  {name + " - Narudžba"}
-                </h1>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                  onClick={handleClose}
-                ></button>
-              </div>
-              <div className="modal-body">{description}</div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                  onClick={handleClose}
-                >
-                  Close
-                </button>
-                <button type="button" className="btn btn-primary">
-                  Save changes
-                </button>
-              </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{name + " - narudžba"}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="d-flex flex-column ">
+            <div className="row justify-content-center align-items-center">
+              <img src={imageUrl} alt={name} className="img-fluid w-75" />
+            </div>
+            <div className="">{description}</div>
+            <hr />
+            <div className="d-flex">
+              <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="4" controlId="validationCustom01">
+                    <Form.Label>First name</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder="First name"
+                      defaultValue="Mark"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="4" controlId="validationCustom02">
+                    <Form.Label>Last name</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      placeholder="Last name"
+                      defaultValue="Otto"
+                    />
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group
+                    as={Col}
+                    md="4"
+                    controlId="validationCustomUsername"
+                  >
+                    <Form.Label>Username</Form.Label>
+                    <InputGroup hasValidation>
+                      <InputGroup.Text id="inputGroupPrepend">
+                        @
+                      </InputGroup.Text>
+                      <Form.Control
+                        type="text"
+                        placeholder="Username"
+                        aria-describedby="inputGroupPrepend"
+                        required
+                      />
+                      <Form.Control.Feedback type="invalid">
+                        Please choose a username.
+                      </Form.Control.Feedback>
+                    </InputGroup>
+                  </Form.Group>
+                </Row>
+                <Row className="mb-3">
+                  <Form.Group as={Col} md="6" controlId="validationCustom03">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control type="text" placeholder="City" required />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide a valid city.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="3" controlId="validationCustom04">
+                    <Form.Label>State</Form.Label>
+                    <Form.Control type="text" placeholder="State" required />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide a valid state.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="3" controlId="validationCustom05">
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control type="text" placeholder="Zip" required />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide a valid zip.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                </Row>
+                <Form.Group className="mb-3">
+                  <Form.Check
+                    required
+                    label="Agree to terms and conditions"
+                    feedback="You must agree before submitting."
+                    feedbackType="invalid"
+                  />
+                </Form.Group>
+                <Button type="submit">Submit form</Button>
+              </Form>
             </div>
           </div>
-        </motion.div>
-      )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </motion.div>
   );
 }
