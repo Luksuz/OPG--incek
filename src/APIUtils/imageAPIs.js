dbd95272d567218d90aa3c71cbdd9e49b9763f07
Luksuz <lukamindjek@gmail.com>
2024-01-10 16:31:25 +0100

    headers change

--- src/APIUtils/imageAPIs.js
+++ src/APIUtils/imageAPIs.js
@@ -29,7 +29,6 @@ async function deleteImage(imageId, token) {
 }}
 
 async function uploadImage(imageFile, name, price, description, token) {
-
     let formData = new FormData();
 
     formData.append('image', imageFile);
@@ -40,12 +39,10 @@ async function uploadImage(imageFile, name, price, description, token) {
 
     const response = await fetch(`https://eipf2exv0c.execute-api.us-east-1.amazonaws.com/sincek/images`, {
         method: "POST",
-        headers: {
-            "Content-Type": "multipart/form-data",
-        },
         body: formData,
     });
     return response.json();
+}
 }
 
 
