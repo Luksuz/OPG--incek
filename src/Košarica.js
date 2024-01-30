e6f4bcad4cdedacd9f91fa9e92c85c526f568a19
Luksuz <lukamindjek@gmail.com>
2024-01-30 09:35:42 +0100

    dostava

--- src/Košarica.js
+++ src/Košarica.js
@@ -8,7 +8,15 @@ import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 
 export default function Košarica() {
-  const [userData, setUserData] = useState({name: "", surname: "", email: "", phone: "", city: "", state: "", zip: ""});
+  const [userData, setUserData] = useState({
+    name: "",
+    surname: "",
+    email: "",
+    phone: "",
+    city: "",
+    state: "",
+    zip: "",
+  });
   const [quantity, setQuantity] = useState(0);
   const [cart, setCart] = useState([]);
   const [show, setShow] = useState(false);
@@ -35,18 +43,22 @@ export default function Košarica() {
     e.preventDefault();
     console.log(userData);
     console.log(cart);
-    const response = await fetch("https://eipf2exv0c.execute-api.us-east-1.amazonaws.com/sincek/invoice", {
-      method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ // Convert the JavaScript object to a JSON string
-        "userData": userData,
-        "cart": cart,
-        "totalPrice": totalPrice,
-        "date": new Date().toISOString() // Convert the date to a string that can be sent in JSON
-      })
-    });
+    const response = await fetch(
+      "https://eipf2exv0c.execute-api.us-east-1.amazonaws.com/sincek/invoice",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          // Convert the JavaScript object to a JSON string
+          userData: userData,
+          cart: cart,
+          totalPrice: totalPrice,
+          date: new Date().toISOString(), // Convert the date to a string that can be sent in JSON
+        }),
+      }
+    );
     //eslint-disable-next-line
     const data = await response.json();
     clearCart();
@@ -64,7 +76,7 @@ export default function Košarica() {
     }
     setQuantity(quantity - 1);
   };
-  
+
   function clearCart() {
     localStorage.removeItem("cart");
     setCart([]);
@@ -83,13 +95,21 @@ export default function Košarica() {
     <div>
       <Navbar />
       {showAlert && (
-              <Alert className="position-absolute w-100 z-5" variant="success" onClose={() => setShow(false)} dismissible>
-             Pdf račun uspješno poslan na email adresu: {userData.email}
-            </Alert>
-            )}
-      <p className="text-center display-4" style={{color: "#73F28F"}}>{cart.length === 0 && "Košarica je prazna"}</p>
-      {cart && cart.map((item, index) => {
-          return(
+        <Alert
+          className="position-absolute w-100 z-5"
+          variant="success"
+          onClose={() => setShow(false)}
+          dismissible
+        >
+          Pdf račun uspješno poslan na email adresu: {userData.email}
+        </Alert>
+      )}
+      <p className="text-center display-4" style={{ color: "#73F28F" }}>
+        {cart.length === 0 && "Košarica je prazna"}
+      </p>
+      {cart &&
+        cart.map((item, index) => {
+          return (
             <Row
               key={index}
               className="d-flex justify-content-between align-items-center text-center"
@@ -105,7 +125,9 @@ export default function Košarica() {
               </Col>
               <Col>
                 <p className="fs-4">Količina: {item.quantity}</p>
-                <p className="fs-4">Ukupna cijena: {item.price * item.quantity}</p>
+                <p className="fs-4">
+                  Ukupna cijena: {item.price * item.quantity}
+                </p>
               </Col>
               <Col>
                 <div className="d-flex gap-4 justify-content-center ">
@@ -124,10 +146,9 @@ export default function Košarica() {
                 </div>
               </Col>
             </Row>
-          )
-        })
-      }
-      
+          );
+        })}
+
       <Row>
         <Col md={10} className="d-flex justify-content-end m-5">
           <button onClick={clearCart} className="btn btn-danger">
@@ -163,14 +184,20 @@ export default function Košarica() {
       {/* Modal */}
       <Modal show={show} onHide={handleClose} className="modal-xl">
         <Modal.Body className="p-0">
-
           <Row className="d-flex m-3">
-            <Col lg={6} sm={12} className="row justify-content-center align-items-center">
-              <img src="delivery.jpeg" alt="delivery" className="img-fluid" />
+            <Col
+              lg={6}
+              sm={12}
+              className="row justify-content-center align-items-center"
+            >
+              <img src="delivery.webp" alt="delivery" className="img-fluid" />
             </Col>
             <Col lg={6} sm={12} className="mt-4">
               <h5 className="card-title">Dostava</h5>
-                <p>Besplatna dostava se vrši u krugu 15 kilometara od nas, a za sve ostale narudžbe, potrebno je osobno podići robu. </p>
+              <p>
+                Besplatna dostava se vrši u krugu 15 kilometara od nas, a za sve
+                ostale narudžbe, potrebno je osobno podići robu.{" "}
+              </p>
               <hr />
               <div className="d-flex">
                 <Form noValidate validated={validated}>
@@ -188,7 +215,9 @@ export default function Košarica() {
                         placeholder="First name"
                         defaultValue="Mark"
                         value={userData.name}
-                        onChange={(e) => setUserData({...userData, name: e.target.value})}
+                        onChange={(e) =>
+                          setUserData({ ...userData, name: e.target.value })
+                        }
                       />
                       <Form.Label>Prezime</Form.Label>
                       <Form.Control
@@ -197,7 +226,9 @@ export default function Košarica() {
                         placeholder="Last name"
                         defaultValue="Otto"
                         value={userData.surname}
-                        onChange={(e) => setUserData({...userData, surname: e.target.value})}
+                        onChange={(e) =>
+                          setUserData({ ...userData, surname: e.target.value })
+                        }
                       />
                       <Form.Control.Feedback>Odlično!</Form.Control.Feedback>
                     </Form.Group>
@@ -211,7 +242,15 @@ export default function Košarica() {
                       className="w-50"
                     >
                       <Form.Label>Email</Form.Label>
-                      <Form.Control type="text" placeholder="Email" value={userData.email} onChange={(e) => setUserData({...userData, email: e.target.value})} required />
+                      <Form.Control
+                        type="text"
+                        placeholder="Email"
+                        value={userData.email}
+                        onChange={(e) =>
+                          setUserData({ ...userData, email: e.target.value })
+                        }
+                        required
+                      />
                       <Form.Control.Feedback type="invalid">
                         Unesite važeći email
                       </Form.Control.Feedback>
@@ -228,7 +267,9 @@ export default function Košarica() {
                         type="text"
                         placeholder="Phone number"
                         value={userData.phone}
-                        onChange={(e) => setUserData({...userData, phone: e.target.value})}
+                        onChange={(e) =>
+                          setUserData({ ...userData, phone: e.target.value })
+                        }
                         required
                       />
                       <Form.Control.Feedback type="invalid">
@@ -245,10 +286,15 @@ export default function Košarica() {
                       className="w-25"
                     >
                       <Form.Label>Grad</Form.Label>
-                      <Form.Control type="text" placeholder="City"
-                      value={userData.city}
-                      onChange={(e) => setUserData({...userData, city: e.target.value})}
-                       required />
+                      <Form.Control
+                        type="text"
+                        placeholder="City"
+                        value={userData.city}
+                        onChange={(e) =>
+                          setUserData({ ...userData, city: e.target.value })
+                        }
+                        required
+                      />
                       <Form.Control.Feedback type="invalid">
                         Unesite važeći grad
                       </Form.Control.Feedback>
@@ -260,10 +306,15 @@ export default function Košarica() {
                       className="w-25"
                     >
                       <Form.Label>Županija</Form.Label>
-                      <Form.Control type="text" placeholder="State"
-                      value={userData.state}
-                      onChange={(e) => setUserData({...userData, state: e.target.value})}
-                       required />
+                      <Form.Control
+                        type="text"
+                        placeholder="State"
+                        value={userData.state}
+                        onChange={(e) =>
+                          setUserData({ ...userData, state: e.target.value })
+                        }
+                        required
+                      />
                       <Form.Control.Feedback type="invalid">
                         Unesite važeću Županiju
                       </Form.Control.Feedback>
@@ -275,10 +326,15 @@ export default function Košarica() {
                       className="w-25"
                     >
                       <Form.Label>Poštanski broj</Form.Label>
-                      <Form.Control type="text" placeholder="Zip"
-                      value={userData.zip}
-                      onChange={(e) => setUserData({...userData, zip: e.target.value})}
-                      required />
+                      <Form.Control
+                        type="text"
+                        placeholder="Zip"
+                        value={userData.zip}
+                        onChange={(e) =>
+                          setUserData({ ...userData, zip: e.target.value })
+                        }
+                        required
+                      />
                       <Form.Control.Feedback type="invalid">
                         Unesite važeči poštanski broj
                       </Form.Control.Feedback>
@@ -295,21 +351,23 @@ export default function Košarica() {
                       feedbackType="invalid"
                     />
                   </Form.Group>
-
-                  <button
-                    className="position-absolute m-4 w-25 rounded-5 bottom-0 start-0 green-button"
-                    style={{ backgroundColor: "#F28F8F" }}
-                    onClick={handleClose}
-                  >
-                    Zatvori
-                  </button>
-                  <button
-                    className="position-absolute m-4 w-25 rounded-5 bottom-0 end-0 green-button"
-                    onClick={handleSubmit}
-                    type="submit"
-                  >
-                    U košaricu
-                  </button>
+                  <div className="d-flex bottom-0 justify-content-between">
+                    <button
+                      className=" m-4 w-25 rounded-5"
+                      style={{ backgroundColor: "#F28F8F" }}
+                      onClick={handleClose}
+                    >
+                      Zatvori
+                    </button>
+                    <button
+                      className="m-4 w-25 rounded-5"
+                      onClick={handleSubmit}
+                      type="submit"
+                      style={{ backgroundColor: "#73F28F" }}
+                    >
+                      U košaricu
+                    </button>
+                  </div>
                 </Form>
               </div>
             </Col>
