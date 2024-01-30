57827e9eb7772cce24f286427ba8240b30c48266
Luksuz <lukamindjek@gmail.com>
2024-01-30 10:49:52 +0100

    navbar fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row';
 import "../styles.css";
 
 export default function NavigationBar() {
@@ -18,24 +19,29 @@ export default function NavigationBar() {
         setQuantity(cartItems);
     }, []);
 
+
     return (
         <Navbar collapseOnSelect expand="md" className="shadow sticky-top mb-5" style={{background: "#73F28F"}}>
-            <Container className='d-flex justify-content-between'>
-                <Navbar.Brand onClick={() => navigate("/")} className='fs-1 me-5'>OPG Šincek</Navbar.Brand>
+            <Container className='d-flex'>
+
+                <Navbar.Brand onClick={() => navigate("/")} className='d-flex align-items-center w-75'>
+                    <img src="logo.png" alt="logo" className='logo img-fluid' width={"10%"}/>
+                    <h1>Vrtne sadnice Šincek</h1>
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav" className='ms-5'>
-                    <Nav className="me-auto d-flex align-items-end fs-5">
+                <Navbar.Collapse id="responsive-navbar-nav" className='ms-5 align-items-center w-100'>
+                    <Nav className="me-auto d-flex align-items-center fs-4">
                         <Nav.Link as={Link} to="/">Početna</Nav.Link>
-                        <Nav.Link as={Link} to="/ONama">O nama</Nav.Link>
+                        <Nav.Link as={Link} to="/ONama">O{" "}nama</Nav.Link>
                         <Nav.Link as={Link} to="/Kontakt">Kontakt</Nav.Link>
                         <Nav.Link as={Link} to="/Ponuda">Ponuda</Nav.Link>
-                        <Nav.Link as={Link} to="/Kosarica" className='ms-5'>
+                        <Nav.Link as={Link} to="/Kosarica">
                             <img src="shopping-cart.png" alt="cart" />
-                            <span className="cart-icon">{quantity}</span>
                     </Nav.Link>
                     </Nav>
                    
                 </Navbar.Collapse>
+
             </Container>
         </Navbar>
     );
